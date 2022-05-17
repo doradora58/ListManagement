3e47f4004fe0bdba4be403c899763743f6ff9054
doradora58 <koudai58@gmail.com>
2022-05-18 01:55:22 +0900

    CSVファイル書き出しクラスの追加

---
+++ ListManagement/WriteFile.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "WriteFile.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+CWriteFile::CWriteFile()
+{
+}
+
+/// <summary>
+/// デスストラクタ
+/// </summary>
+CWriteFile::~CWriteFile()
+{
+}
---
+++ ListManagement/WriteFile.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// CSVファイル書き出しクラス
+class CWriteFile
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	CWriteFile();
+
+	/// <summary>
+	/// デスストラクタ
+	/// </summary>
+	~CWriteFile();
+
+};
