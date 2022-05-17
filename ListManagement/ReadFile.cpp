58e0caa911b7418e8c41f4006eb0abe1cb7f277e
洋平 山内 <y.yamauchi12078@gmail.com>
2022-05-18 01:47:32 +0900

    csvファイル読み込みクラス追加

---
+++ ListManagement/ReadFile.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "ReadFile.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+CReadFile::CReadFile()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+CReadFile::~CReadFile()
+{
+}
---
+++ ListManagement/ReadFile.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// csv読み込みクラス
+class CReadFile
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	CReadFile();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~CReadFile();
+
+};
