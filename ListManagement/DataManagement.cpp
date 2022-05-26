2a15ec395ed3c941567f436b7ee70089c83d40fa
洋平 山内 <y.yamauchi12078@gmail.com>
2022-05-27 00:42:32 +0900

    aa

--- ListManagement/DataManagement.cpp
+++ ListManagement/DataManagement.cpp
@@ -354,6 +354,9 @@ int CDataManagement::ReadFileDataKK(TCHAR cReadPath[MAX_PATH])
 		// CDataInfoインスタンスの作成
 		CDataInfo* cDataInfo = new CDataInfo();
 		m_pacDataInfo->Add(cDataInfo);
+		// 
+//	}
+
 		cDataInfo->SetData(
 			CStringArrayList.ElementAt(i)->ElementAt(0),
 			CStringArrayList.ElementAt(i)->ElementAt(1),
